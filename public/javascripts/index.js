331a67514e832627ba7337d65f0740326c66f9af
Duncan Angus Wilkie (mrdnk) <duncanwilkie@hotmail.com>
2013-03-07 19:51:47 +0000

    API changes: New done callback, and soundy.sounds[name] hash of sounds and soundy.play(name) interfaces

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,85 +1,102 @@
-var context
-window.addEventListener('load', init, false)
-function init () {
-	try {
-		context = new webkitAudioContext() || new mozAudioContext() || new AudioContext()
-	}
-	catch (e) {
-		alert('Web Audio API is not supported in your browser')
-	}
-}
+var soundy = (function () {
 
-var animalList = {}
+	var that = {}
+	that.sounds = {}
 
-function loadSound (url, name, cb) {
-	var request = new XMLHttpRequest ()
-	request.open ('GET', url, true)
-	request.responseType = 'arraybuffer'
+	var context
 
-	// Decode asynchronously
-	request.onload = function () {
-		context.decodeAudioData (request.response, function (buffer) {
-			animalList[name] = buffer
-			cb(null, 1)
-		}, onError)
-	}
-	request.send ();
-}
-
-function onError (e) {
-	console.error(e)
-}
-
-function playSound (buffer) {
-	var source = context.createBufferSource ()
-	source.buffer = buffer
-	source.connect (context.destination)
-	source.noteOn(0)
-}
-
-function loadLibrary (cb) {
-	var sounds = [
-		{
-				file: 'German-Reject1.wav'
-			,	name: 'german'
+	function init () {
+		try {
+			context = new webkitAudioContext() || new mozAudioContext() || new AudioContext()
 		}
-		,	{
-				file: 'Moo-Panicked.wav'
-			,	name: 'moo'
+		catch (e) {
+			alert('Web Audio API is not supported in your browser')
 		}
-		,	{
-				file: 'Baa1.wav'
-			,	name: 'baa'
-		}
-		, {
-				file: 'Dog1.wav'
-			,	name: 'dog'
-		}
-		,	{
-				file: 'Dog2.wav'
-			,	name: 'dog2'
-		}
-	];
-
-	var loadedCount = 0
-
-	for(var i=0; i<sounds.length; i++) {
-		loadSound('/audio/' + sounds[i].file, sounds[i].name, function (err, done) {
-			loadedCount ++
-			if (sounds.length === loadedCount) 
-				cb(null, 'loaded')
-		});
-	}
-}
-
-loadLibrary( function (err, loaded) {
-	if(loaded) {
-		console.log('sounds loaded')
-		//playSound(animalList.german)
-		//playSound(animalList.dog)
-		//playSound(animalList.baa)
-		playSound(animalList.dog2)
 	}
 
-});
+	var animalList = {}
 
+	function loadSound (url, name, cb) {
+		var request = new XMLHttpRequest ()
+		request.open ('GET', url, true)
+		request.responseType = 'arraybuffer'
+
+		// Decode asynchronously
+		request.onload = function () {
+			context.decodeAudioData (request.response, function (buffer) {
+				animalList[name] = buffer
+				that.sounds[name] = name
+				cb(null, 1)
+			}, cb)
+		}
+		request.send ();
+	}
+
+	function onError (e) {
+		console.error(e)
+	}
+
+	function playSound (buffer) {
+		var source = context.createBufferSource ()
+		source.buffer = buffer
+		source.connect (context.destination)
+		source.noteOn(0)
+	}
+
+	function play (name) {
+		playSound(animalList[name])
+	}
+
+	function loadLibrary (cb) {
+		var sounds = [
+			{
+					file: 'German-Reject1.wav'
+				,	name: 'german'
+			}
+			,	{
+					file: 'Moo-Panicked.wav'
+				,	name: 'moo'
+			}
+			,	{
+					file: 'Baa1.wav'
+				,	name: 'baa'
+			}
+			, {
+					file: 'Dog1.wav'
+				,	name: 'dog'
+			}
+			,	{
+					file: 'Dog2.wav'
+				,	name: 'dog2'
+			}
+		];
+
+		var loadedCount = 0
+
+		for(var i=0; i<sounds.length; i++) {
+			loadSound('/audio/' + sounds[i].file, sounds[i].name, function (err, done) {
+				loadedCount ++
+				if (sounds.length === loadedCount) 
+					cb(null, 'loaded')
+			});
+		}
+	}
+
+	window.addEventListener('load', init, false)
+
+	function add (file, name) {
+		console.log(file, name);	
+	}
+
+	that.done = loadLibrary
+	that.add = add
+	that.play = play
+
+	return that
+
+}())
+
+soundy.done( function (err, done) {
+	console.log(err, done)
+	soundy.play(soundy.sounds.german)
+})
